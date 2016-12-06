21b255078541671e726dc5ca5035dbf46e613852
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2016-12-06 14:59:50 -0500

    name

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject com.ambrosebs.fast-attr "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Exploration in Hash Array Mapped Tries."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
