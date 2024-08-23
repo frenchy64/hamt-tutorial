6e767fd3ff6d6a04dfddf49a8b79725a23cd362c
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-08-23 15:13:23 -0500

    deps

--- project.clj
+++ project.clj
@@ -2,10 +2,9 @@
   :description "Exploration in Hash Array Mapped Tries."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.3"]
-                 [criterium "0.4.4"]
-                 [potemkin "0.4.3"]
-                 [rhizome "0.2.7"]]
+  :dependencies [[org.clojure/clojure "1.11.3"]]
   :profiles {:dev {:dependencies [[collection-check "0.1.6"]
-                                  [org.clojure/test.check "0.9.0"]]}}
+                                  [criterium "0.4.4"]
+                                  [rhizome "0.2.7"]
+                                  [org.clojure/test.check "1.1.1"]]}}
   :java-source-paths ["java"])
